0fb92aa4d682f924ca5fa7c31f3d31e96741d08e
user <converttorgb@gmail.com>
2020-07-14 19:27:30 +0300

    edibo

---
+++ 14_07_2020/dev/MySecondApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MySecondApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        var txt = "EDIBO"; 
+        int num = 123;
+	    Console.WriteLine($"Hello World! {txt} RTU 2020");
+        }
+    }
+}
