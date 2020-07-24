09a7ea70d8bc2f6c6978f8a8c647bc272330564f
converttorgb <converttorgb@gmail.com>
2020-07-24 19:47:28 +0300

    upd

---
+++ 24_07_2020/toDo/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
